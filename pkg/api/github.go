41e1edd7103e26a18dc80252c4bb96d90c35c8b9
Thibault Jamet <thibault.jamet@adevinta.com>
2023-10-05 11:37:46 +0200

    bump github client version
    Change-Id: Ic25a3f1b15701190261b8834616a344b3b359c70

--- pkg/api/github.go
+++ pkg/api/github.go
@@ -11,7 +11,7 @@ import (
 	gh "github.com/adevinta/maiao/pkg/github"
 	"github.com/adevinta/maiao/pkg/log"
 	"github.com/go-git/go-git/v5/plumbing/transport"
-	"github.com/google/go-github/v40/github"
+	"github.com/google/go-github/v55/github"
 	"github.com/sirupsen/logrus"
 )
 
