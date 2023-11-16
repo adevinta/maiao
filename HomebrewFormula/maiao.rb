c2775cfcbbb42a6cdf94cb57fa13e89fe7c0f9fd
Thibault Jamet <tjamet@users.noreply.github.com>
2023-11-16 14:58:29 +0100

    Bump version in homebrew's formula
    Change-Id: I2a6f895c9575c0cde68b033d8a59e7a8a983d8cb

--- HomebrewFormula/maiao.rb
+++ HomebrewFormula/maiao.rb
@@ -2,8 +2,8 @@ class Maiao < Formula
   desc "Seamless GitHub PR management from the command-line"
   homepage "https://github.com/adevinta/maiao"
   url "https://github.com/adevinta/maiao.git",
-    tag:      "1.1.0",
-    revision: "a105d869324d20a9c94081cb0f03149a743e184b"
+    tag:      "1.2.0",
+    revision: "bf4f552a17c69bd741b5f1fdfc7c725828c4b1b0"
   license "MIT"
   conflicts_with "git-review"
   head "https://github.com/adevinta/maiao.git",
